1fbc0574937b83617f9b105d209bc84f222ad100
nomadsomni <ayotusa@gmail.com>
2024-04-07 23:35:09 +0200

    01.1

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const Paragraph = () =>{
+    return React.createElement(
+        'p',
+        {
+            style: {
+                backgroundColor: 'blue',
+                fontSize: 'bold',
+            },
+            id: 'first-paragraph',
+            className: 'paragraph',
+        },
+        'lorem ipsum dolor sit amet',
+    );
+};
+
+
+const appContainer = document.querySelector('#app');
+
+ReactDOM.render(Paragraph(), appContainer);
