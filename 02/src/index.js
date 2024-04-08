a806364b90bf378bf8f7a97ac3abb4c45ea8adda
nomadsomni <ayotusa@gmail.com>
2024-04-08 23:20:15 +0200

    02.2

--- 02/src/index.js
+++ 02/src/index.js
@@ -1,6 +1,8 @@
 import { data } from "./data.js";
 import { combineAge , renderAge} from "./helpers.js";
+import camelCase from 'lodash/camelCase';
 
+console.log(camelCase('hello roman co tam slychac'));
 console.log(combineAge(data));
 const container = document.querySelector('#age');
 renderAge(container, data);
